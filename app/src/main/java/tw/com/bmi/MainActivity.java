b120b31c492b66fff9f5b18f1dfa45ed40723caa
qqww81407 <qqww81407@yahoo.com.tw>
2021-07-23 21:36:13 +0800

    BMI計算完成

--- app/src/main/java/tw/com/bmi/MainActivity.java
+++ app/src/main/java/tw/com/bmi/MainActivity.java
@@ -3,12 +3,34 @@ package tw.com.bmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView result; //顯示計算結果用
+    private EditText weight,height;  //讓使用者輸入數值用
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //將布局檔載入
+
+        result = findViewById(R.id.result);  //定義布局檔中的元件
+        weight = findViewById(R.id.userweight);
+        height = findViewById(R.id.userheight);
+
+    }
+    public void bmiClick(View view){ //要讓布局檔呼叫的方法,權限一定要是public
+        float userweight = Float.parseFloat(weight.getText().toString());
+        float userheight = Float.parseFloat(height.getText().toString());
+        float bmi = userweight / ((userheight/100) * (userheight/100));
+
+        result.setText("結果:"+bmi);
+
     }
 }
